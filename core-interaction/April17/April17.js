e7c1daa6bda7d1646966d53a07e48f2c5b6bb3bc
kangs041 <31454092+kangs041@users.noreply.github.com>
2018-04-17 21:07:03 -0400

    april17

---
+++ core-interaction/April17/April17.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("keypress",handleKeyPress)
+
+function handleKeyPress(evt) {
+		document.getElementById("blue").style.backgroundImage="blue.jpeg"
+
+
+		if (document.getElementById("blue",).style.backgroundColor =="blueplace.jpeg") {
+
+document.getElementById("blue").style.backgroundColor = "redplace.jpeg"
+} else {
+document.getElementById("blue").style.backgroundColor = "blueplace.jpeg"
+
+}	}
