e270c1bed72e1a00bd606ac68830ce70375c72b1
kangs041 <31454092+kangs041@users.noreply.github.com>
2018-05-10 19:12:55 -0400

    ss

--- core-interaction/site interface/location.js
+++ core-interaction/site interface/location.js
@@ -1,7 +1,7 @@
-// touch events are very similar to mouse events
 
 
-var gray = 0;
+
+
 
 var setup = function() {
 	createGraphics(1000, 1650);
@@ -11,9 +11,49 @@ var setup = function() {
 		document.body.style.backgroundSize = "1000px 2000px";
 };
 
-var draw = function() {  // Empty draw() keeps the program running
+var draw = function() {
+  // keeps program looping even though it is empty
 };
 
-var mouseReleased = function() {
-	rect(mouseX, mouseY, 50, 50);
+// define functions used for both touch and mouse
+var drawGreen = function(x, y) {
+  fill(0, 255, 0);
+  ellipse(x, y, 80, 80);
 };
+
+
+var drawRed = function(x, y) {
+  fill(255, 0, 0);
+  ellipse(x, y, 80, 80);
+};
+
+var drawBlue = function(x, y) {
+  fill(0, 0, 255);
+  ellipse(x, y, 80, 80);
+};
+
+// hook functions up to touches
+var touchStarted = function() {
+  drawGreen(touchX, touchY);
+}
+
+var touchMoved = function() {
+  drawBlue(touchX, touchY);
+}
+
+var touchEnded = function() {
+  drawRed(touchX, touchY);
+}
+
+// hook functions up to mouse
+var mousePressed = function() {
+  drawGreen(mouseX, mouseY);
+}
+
+var mouseDragged = function() {
+  drawBlue(mouseX, mouseY);
+}
+
+var mouseReleased = function() {
+  drawRed(mouseX, mouseY);
+}
