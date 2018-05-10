acaa40a068e001bc319eb86b295ecc23925c97ed
kangs041 <31454092+kangs041@users.noreply.github.com>
2018-05-10 19:08:43 -0400

    a

--- core-interaction/site interface/location.js
+++ core-interaction/site interface/location.js
@@ -7,7 +7,7 @@ var setup = function() {
 	createGraphics(1000, 1650);
 	fill(255, 255, 0);
 	noStroke();
-    document.body.style.backgroundImage = "url('map.jpg')";
+    document.body.style.backgroundImage = "map.jpg";
 		document.body.style.backgroundSize = "1000px 2000px";
 };
 
